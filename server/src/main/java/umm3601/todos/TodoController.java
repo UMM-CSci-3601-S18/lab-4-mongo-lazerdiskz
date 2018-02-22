ce1883393a2b7b4e0f8a8fa4133e071cf27a2184
Francisco Montanez <monta215@morris.umn.edu>
2018-02-22 14:15:50 -0600

    create main todo classes to outline functionality for the todos.

---
+++ server/src/main/java/umm3601/todos/TodoController.java
@@ -0,0 +1,16 @@
+package umm3601.todos;
+
+import com.mongodb.client.MongoDatabase;
+
+public class TodoController {
+
+    public TodoController(MongoDatabase database) {
+
+    }
+
+    public String getTodo(String id) {
+        return null;
+    }
+
+
+}
