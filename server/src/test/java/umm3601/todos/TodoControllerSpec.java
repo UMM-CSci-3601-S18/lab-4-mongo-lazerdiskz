7eaf50bd9a111f77f9ff7b5dee3523d05bab88bf
Francisco Montanez <monta215@morris.umn.edu>
2018-02-22 14:58:15 -0600

    add blank tests for server

--- server/src/test/java/umm3601/todos/TodoControllerSpec.java
+++ server/src/test/java/umm3601/todos/TodoControllerSpec.java
@@ -1,4 +1,82 @@
 package umm3601.todos;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.*;
+import org.bson.codecs.*;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.json.JsonReader;
+import org.bson.types.ObjectId;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+
 public class TodoControllerSpec {
+    private TodoController todoController;
+
+    @Before
+    public void clearAndPopulateDB() throws IOException {
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase db = mongoClient.getDatabase("test");
+        MongoCollection<Document> todoDocuments = db.getCollection("todos");
+        todoDocuments.drop();
+        List<Document> testTodos = new ArrayList<>();
+
+        // add insertions of todos
+
+        // this needs to be below the rest of the before method.
+        todoController = new TodoController(db);
+    }
+
+    private BsonArray parseJsonArray(String json) {
+        final CodecRegistry codecRegistry
+            = CodecRegistries.fromProviders(Arrays.asList(
+            new ValueCodecProvider(),
+            new BsonValueCodecProvider(),
+            new DocumentCodecProvider()));
+
+        JsonReader reader = new JsonReader(json);
+        BsonArrayCodec arrayReader = new BsonArrayCodec(codecRegistry);
+
+        return arrayReader.decode(reader, DecoderContext.builder().build());
+    }
+
+    private static String getOwner(BsonValue val) {
+        BsonDocument doc = val.asDocument();
+        return ((BsonString) doc.get("owner")).getValue();
+    }
+
+    @Test
+    public void getAllTodos() {
+        Map<String, String[]> emptyMap = new HashMap<>();
+        String jsonResult = todoController.getTodos(emptyMap);
+        BsonArray docs = parseJsonArray(jsonResult);
+
+        // add test assertions
+    }
+
+    @Test
+    public void getCompleteTodos() {
+        Map<String, String[]> argMap = new HashMap<>();
+
+        // add assertions and arguments for argMap
+    }
+
+    @Test
+    public void addTodoTest() {
+        // add body of method
+    }
+
+    @Test
+    public void getTodoByCategory() {
+        Map<String, String[]> argMap = new HashMap<>();
+        // add body of method
+    }
 }
