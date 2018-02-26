8310fbafb83d6a4cf3571637efb3362436508079
corde171 <corde171@morris.umn.edu>
2018-02-26 08:43:40 -0600

    Change search todo variable names in todo-list.component.ts and .html
    Add method body for todo-list.component.ts

--- client/src/app/todos/todo-list.component.ts
+++ client/src/app/todos/todo-list.component.ts
@@ -3,6 +3,7 @@ import {Todo} from "./todo";
 import {TodoListService} from "./todo-list.service";
 import {MatDialog} from '@angular/material';
 import {Observable} from 'rxjs/Observable';
+import {AddTodoComponent} from "./add-todo.component";
 
 
 @Component({
@@ -18,21 +19,84 @@ export class TodoListComponent implements OnInit {
 
     // These are the target values used in searching.
     // We should rename them to make that clearer.
-    public todoID: string;
-    public todoOwner: string;
-    public todoStatus: boolean;
-    public todoCategory: string;
-    public todoBody: string;
+    public searchTodoID: string;
+    public searchTodoOwner: string;
+    public searchTodoStatus: boolean;
+    public searchTodoCategory: string;
+    public searchTodoBody: string;
+
+    private highlightedID: {'$oid': string} = { '$oid': '' };
 
     constructor(public todoListService: TodoListService, public dialog: MatDialog) {
 
+    }
+
+    isHighlighted(todo: Todo): boolean {
+        return todo._id['$oid'] === this.highlightedID['$oid'];
+    }
+
+    openDialog(): void {
+        const newTodo: Todo = {_id: '', owner: '', status: false, body: '', category: ''};
+        const dialogRef = this.dialog.open(AddTodoComponent, {
+            width: '500px',
+            data: { user: newTodo }
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            this.todoListService.addNewTodo(result).subscribe(
+                result => {
+                    this.highlightedID = result;
+                    this.refreshTodos();
+                },
+                err => {
+                    // This should probably be turned into some sort of meaningful response.
+                    console.log('There was an error adding the user.');
+                    console.log('The error was ' + JSON.stringify(err));
+                });
+        });
     }
 
     public filterTodos(searchOwner: string, searchCategory: string, searchBody: string, searchStatus: boolean) {
         this.filteredTodos = this.todos;
     }
 
-    ngOnInit(): void {
+    refreshTodos(): Observable<Todo[]> {
+        // Get Users returns an Observable, basically a "promise" that
+        // we will get the data from the server.
+        //
+        // Subscribe waits until the data is fully downloaded, then
+        // performs an action on it (the first lambda)
 
+        const todos: Observable<Todo[]> = this.todoListService.getTodos();
+        todos.subscribe(
+            todos => {
+                this.todos = todos;
+                this.filterTodos(this.searchTodoOwner,
+                    this.searchTodoCategory,
+                    this.searchTodoBody,
+                    this.searchTodoStatus);
+            },
+            err => {
+                console.log(err);
+            });
+        return todos;
+    }
+
+
+    loadService(): void {
+        this.todoListService.getTodos(this.searchTodoCategory).subscribe(
+            todos => {
+                this.todos = todos;
+                this.filteredTodos = this.todos;
+            },
+            err => {
+                console.log(err);
+            }
+        );
+    }
+
+    ngOnInit(): void {
+        this.refreshTodos();
+        this.loadService();
     }
 }
