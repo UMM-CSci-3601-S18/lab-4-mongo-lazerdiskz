f465b1dba689cfb0c73e6aa24db1f2bdc622328d
corde171 <corde171@morris.umn.edu>
2018-02-27 13:26:07 -0600

    Modify tests for todo-list.service.ts and its methods

--- client/src/app/todos/todo-list.service.ts
+++ client/src/app/todos/todo-list.service.ts
@@ -15,8 +15,8 @@ export class TodoListService {
     constructor(private http: HttpClient) {
     }
 
-    getTodos(todoCategory?: string): Observable<Todo[]> {
-        this.filterByCategory(todoCategory);
+    getTodos(todoOwner?: string): Observable<Todo[]> {
+        this.filterbyOwner(todoOwner);
         return this.http.get<Todo[]>(this.todoUrl);
     }
 
@@ -24,37 +24,43 @@ export class TodoListService {
         return this.http.get<Todo>(this.todoUrl + '/' + id);
     }
 
-    filterByCategory(todoCategory?: string): void {
-        if (!(todoCategory == null || todoCategory === '')) {
-            if (this.parameterPresent('category=') ) {
+    filterbyOwner(todoOwner?: string): void {
+        if (!(todoOwner == null || todoOwner === '')) {
+            if (this.parameterPresent('owner=') ) {
                 // there was a previous search by category that we need to clear
-                this.removeParameter('category=');
+                // this is unnecessary because owner is the only criterion we're filtering by
+
+                // this.removeParameter('owner=');
             }
             if (this.todoUrl.indexOf('?') !== -1) {
                 // there was already some information passed in this url
-                this.todoUrl += 'category=' + todoCategory + '&';
+                this.todoUrl += 'owner=' + todoOwner + '&';
             } else {
                 // this was the first bit of information to pass in the url
-                this.todoUrl += '?category=' + todoCategory + '&';
+                this.todoUrl += '?owner=' + todoOwner + '&';
             }
-        } else {
+        }
+        // this method is useless because we're only using the server to filter by owner, no other things
+        /*else {
             // there was nothing in the box to put onto the URL... reset
-            if (this.parameterPresent('category=')) {
-                let start = this.todoUrl.indexOf('category=');
+
+            if (this.parameterPresent('owner=')) {
+                let start = this.todoUrl.indexOf('owner=');
                 const end = this.todoUrl.indexOf('&', start);
                 if (this.todoUrl.substring(start - 1, start) === '?') {
                     start = start - 1;
                 }
                 this.todoUrl = this.todoUrl.substring(0, start) + this.todoUrl.substring(end + 1);
             }
-        }
+        }*/
     }
 
     private parameterPresent(searchParam: string) {
         return this.todoUrl.indexOf(searchParam) !== -1;
     }
 
-    //remove the parameter and, if present, the &
+    // unnecessary if owner is the only criterion we're using
+    /*//remove the parameter and, if present, the &
     private removeParameter(searchParam: string) {
         let start = this.todoUrl.indexOf(searchParam);
         let end = 0;
@@ -64,7 +70,7 @@ export class TodoListService {
             end = this.todoUrl.indexOf('&', start);
         }
         this.todoUrl = this.todoUrl.substring(0, start) + this.todoUrl.substring(end);
-    }
+    }*/
 
     addNewTodo(newTodo: Todo): Observable<{'$oid': string}> {
         const httpOptions = {
